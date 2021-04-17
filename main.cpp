82cfd25807689b77a485f6a698b6e36b5a1b2a64
Pedro Henrique Avelar Borges <>
2021-04-17 17:08:38 +0000

    Adicionei uma nova função para organizar o código.

--- main.cpp
+++ main.cpp
@@ -35,51 +35,50 @@ bool verificador(string code){
     }
 }
 
-int main() {
-    int mestre, n1, n2;
-    string aux, codigo = "", palavra = "";
-    cin >> mestre;
-    if (mestre != 0){
-    
-    
+string formadorDepalavra(int tamanho){
+    int n1, n2;
+    string aux, codigo = "", palavra = ""; 
+    for(int i = 0; i < tamanho; i++){     
+        cin >> codigo;
+        if(verificador(codigo) == true){
+            aux = codigo[1];
+            n1 = stoi(aux) - 2;
 
-        for(int i = 0; i < mestre; i++){
-            cin >> codigo;
-            if(verificador(codigo) == true){
-                aux = codigo[1];
-                n1 = stoi(aux) - 2;
-
-                //Essa parte permite que o usuário aperte o "botão" mais vezes
-                //Como exemplo caso o usuário escolha o numero 2 e no código peça para apertar 4
-                //Vezes, ao invés de ocasionar um bug por ser um numero maior que o tamanho do vector,
-                //Irá simplesmente fazer que ele volte para o inicio, ou seja, se ele colocar 4 irá para
-                //letra "a" de novo, 5 ira para a "b" e assim por diante, deixando a experiência mais
-                //parecida com um celular da época.
-                //Inicio
-                aux = "";
-                for (int n = 3; n < codigo.length(); n++){
-                    aux += codigo[n];
-                }
-                n2 = stoi(aux) - 1;
-                if (n1 != 5 and n1 != 7){
-                    n2 %= 3;
-                }else{
-                    n2 %= 4;
-                }
-                //Fim
-
-                palavra += texto[n1][n2];    
-            }else{
-                cout << "código invalido";
-                palavra = "";
-                break;
+            //Essa parte permite que o usuário aperte o "botão" mais vezes
+            //Como exemplo caso o usuário escolha o numero 2 e no código peça para apertar 4
+            //Vezes, ao invés de ocasionar um bug por ser um numero maior que o tamanho do vector,
+            //Irá simplesmente fazer que ele volte para o inicio, ou seja, se ele colocar 4 irá para
+            //letra "a" de novo, 5 ira para a "b" e assim por diante, deixando a experiência mais
+            //parecida com um celular da época.
+            //Inicio
+            aux = "";
+            for (int n = 3; n < codigo.length(); n++){
+                aux += codigo[n];
             }
-            
+            n2 = stoi(aux) - 1;
+            if (n1 != 5 and n1 != 7){
+                n2 %= 3;
+            }else{
+                n2 %= 4;
+            }
+            //Fim
+
+            palavra += texto[n1][n2];    
+        }else{
+            cout << "código invalido, tente de novo\n";
+            i--;
         }
-    
-        cout << palavra;
+    }
+    return palavra;
+}
+
+int main() {
+    int tamanhoPalavra;
+    string palavra = "";
+    cin >> tamanhoPalavra;
+    if (tamanhoPalavra != 0){
+        cout << formadorDepalavra(tamanhoPalavra);
     }else{
         cout << "Tamanho da palavra inválido";
     }
-
-    }
+}
